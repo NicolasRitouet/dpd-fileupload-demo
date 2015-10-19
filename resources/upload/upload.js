d1f6a5ecfea09c27b340cce6e51805475b18c38c
NicolasRitouet <nicolas@ritouet.com>
2015-10-19 18:18:05 +0200

    Upgrade dpd-fileupload to 0.0.15

---
+++ resources/upload/upload.js
@@ -0,0 +1,6 @@
+
+console.log("originalFilename", originalFilename);
+console.log("filename", filename);
+console.log("uniqueFilename", uniqueFilename);
+console.log("subdir", subdir);
+console.log("filesize", filesize);
